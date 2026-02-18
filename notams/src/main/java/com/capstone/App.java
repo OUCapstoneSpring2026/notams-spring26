47367e8e81d351aff5f2d9077ca5b91692e8f80e
Noah Gibson <noah.gibson.dev@gmail.com>
2026-02-18 10:35:02 -0600

    [CAP-20] Update Airport.java and App.java based on PR feedback. Add README.md with build instructions. Remove all VS Code settings from .gitignore

--- notams/src/main/java/com/capstone/App.java
+++ notams/src/main/java/com/capstone/App.java
@@ -2,16 +2,16 @@ package com.capstone;
 
 import com.capstone.models.Airport;
 
-
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         try {
-            Airport departure = new Airport("KOKC");
-            Airport arrival = new Airport("KDFW");
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            final Airport departure = new Airport( "KOKC" );
+            final Airport arrival = new Airport( "KDFW" );
+        }
+        catch( final Exception e ) {
+            System.out.println( "Error: " + e.getMessage() );
         }
     }
 }
--- notams/src/main/java/com/capstone/models/Airport.java
+++ notams/src/main/java/com/capstone/models/Airport.java
@@ -2,38 +2,54 @@ package com.capstone.models;
 
 import java.awt.geom.Point2D;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
 
-public class Airport {
-    private String icao;
-    private Point2D coords;
+public class Airport
+{
+    private final String icao;
+    private final Point2D coords;
+    private static final String AIRPORT_COORDS_FILENAME = "airportCoords.csv";
 
-    // Constructor
-    public Airport(String icao) throws Exception{
+    public Airport( String icao ) throws IOException
+    {
         this.icao = icao;
-        this.coords = loadAirportCoordsFromCsv(icao);
+        this.coords = loadAirportCoordsFromCsv( icao );
     }
 
-    public String getIcao() { return icao; }
+    public String getIcao()
+    {
+        return icao;
+    }
 
-    public Point2D getCoords() { return coords; }
+    public Point2D getCoords()
+    {
+        return coords;
+    }
 
-    private Point2D loadAirportCoordsFromCsv(String icao) throws Exception {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("airportCoords.csv");
-        if (is == null) throw new Exception("airportCoords.csv not found.");
+    private Point2D loadAirportCoordsFromCsv( String icao ) throws IOException
+    {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(
+                AIRPORT_COORDS_FILENAME );
+        if( is == null )
+            throw new FileNotFoundException( "File not found: "
+                    + AIRPORT_COORDS_FILENAME );
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+        try (final BufferedReader br = new BufferedReader(
+                new InputStreamReader( is ) )) {
             String line;
-            while ((line = br.readLine()) != null) {
+            while( (line = br.readLine()) != null ) {
 
                 // TODO: parse coords from CSV
-                if (true) {
-                    return new Point2D.Double(0, 0);
+                if( true ) {
+                    return new Point2D.Double( 0, 0 );
                 }
             }
         }
 
-        throw new Exception("ICAO coords not found: " + icao);
+        throw new NoSuchElementException( "ICAO coords not found: " + icao );
     }
 }
