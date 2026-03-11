868f55f3c718ee7507545e1524e108adad1cdc33
Chris <mrcdean16@gmail.com>
2026-03-11 10:42:25 -0500

    Add a simple dummy scoring algo based on notam text length

---
+++ notams/src/main/java/com/capstone/services/NotamScoring.java
@@ -0,0 +1,26 @@
+package com.capstone.services;
+
+import com.capstone.models.Notam;
+
+public class NotamScoring
+{
+    /**
+     * DUMMY SCORING ALGORITHM (CAP-29):
+     * Calculates a priority score based on the length of the NOTAM text.
+     * The score is capped at a maximum of 100.
+     * This should work when our Notam class is merged and we have access.
+     * Will update App.java to include this once approved.
+     */
+    public static int calculateScore( Notam notam )
+    {
+        if( notam == null || notam.getText() == null ) {
+            return 0;
+        }
+
+        // Get the char. count of notam text
+        int textLength = notam.getText().length();
+
+        // Return the length as the priority score (capped at 100)
+        return Math.min( textLength, 100 );
+    }
+}
